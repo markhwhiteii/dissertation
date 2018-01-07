32ea5097bba90e83f723023e2679f6aac608230c
Mark White <markhwhiteii@gmail.com>
2018-01-07 12:12:37 -0600

    simple slopes

--- R/study5.R
+++ R/study5.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 auth5 <- read_csv("../data/study5.csv") %>% 
   mutate(
+    cond = as.factor(cond),
     neg_auth = (nowill_auth_gen + nowill_true_honest + 
                   lazy_auth_gen + lazy_true_honest) / 4,
     neg_pc = (nowill_pc + nowill_careful_polite +
@@ -27,11 +28,14 @@ psych::fa(cov(auth5[, 29:36]), nfactors = 2, fm = "pa", rotate = "oblimin")
 
 with(auth5, cor.test(neg_auth, neg_pc))
 with(auth5, cor.test(pos_auth, pos_pc))
+cor(auth5[, c("prej", "neg_auth", "neg_pc")])
+cancor(auth5[, c("prej")], auth5[, c("neg_auth", "neg_pc")])
 
 t.test(prej ~ cond, auth5)
 
 mod1 <- lm(neg_auth ~ prej * cond, auth5)
 summary(mod1)
+summary(lm(neg_auth ~ prej * relevel(cond, "Suppression"), auth5))
 #plot(mod1)
 
 with(auth5, cor.test(neg_auth, prej))
