58eaa62000899c96ca693f97df21daae3dba073f
Mark White <markhwhiteii@gmail.com>
2018-01-07 16:16:40 -0600

    simple slopes, demos

--- R/study7.R
+++ R/study7.R
@@ -2,23 +2,24 @@ library(tidyverse)
 library(ggExtra)
 auth7 <- read_csv("../data/study7.csv") %>% 
   mutate(
+    cond = as.factor(cond),
     auth = (auth1 + auth2 + auth3 + auth4) / 4,
     symrac = (symrac1 + symrac2 + (8 - symrac3) + symrac4 + (8 - symrac5) + 
                 (8 - symrac6) + (8 - symrac7) + symrac8) / 8
   )
 
-summary(auth7[, grepl("symrac", names(auth7))])
+nrow(auth7)
+c(summary(auth7$age), sd(auth7$age))
+prop.table(table(auth7$gender))
+prop.table(table(auth7$race))
 
-psych::fa.parallel(auth7[, c("auth1", "auth2", "auth3", "auth4")], fm = "pa")
-psych::fa.parallel(auth7[, c("symrac1", "symrac2", "symrac3", "symrac4",
-                           "symrac5", "symrac6", "symrac7", "symrac8")], 
-                   fm = "pa")
-psych::fa(auth7[, c("symrac1", "symrac2", "symrac3", "symrac4",
-                  "symrac5", "symrac6", "symrac7", "symrac8")],
-          nfactors = 1, fm = "pa")
+summary(auth7[, grepl("symrac", names(auth7))])
 
 mod <- lm(auth ~ symrac * cond, auth7)
 summary(mod)
+summary(lm(auth ~ symrac * relevel(cond, ref = "Directional"), auth7))
+
+summary(glm(I(auth == 7) ~ symrac * cond, family = binomial, data = auth7))
 
 plot <- ggplot(auth7, aes(x = symrac, y = auth, group = cond, color = cond)) +
   geom_jitter(size = 2) +
@@ -31,8 +32,3 @@ plot <- ggplot(auth7, aes(x = symrac, y = auth, group = cond, color = cond)) +
 ggMarginal(plot, type = "histogram")
 
 cor.test(~ symrac + auth, auth7)
-summary(censReg(auth ~ symrac, 1, 7, auth7))
-
-t.test(auth ~ cond, auth7)
-t.test(time ~ cond, auth7)
-t.test(symrac ~ cond, auth7)
